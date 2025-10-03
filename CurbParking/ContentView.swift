ae29e2f44561c55fb24364166fec71223def0e31
Lau <dfly0603@163.com>
2025-10-03 14:32:23 +0800

    Sample initiated.

---
+++ CurbParking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CurbParking
+//
+//  Created by Lau on 2025/10/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CurbParking/CurbParkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurbParkingApp.swift
+//  CurbParking
+//
+//  Created by Lau on 2025/10/03.
+//
+
+import SwiftUI
+
+@main
+struct CurbParkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
